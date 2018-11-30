7bbdbdc63817a2e33cfb5df1c152f206b6cf7ad3
j_lorenzen <jannik.lorenzen@modix.eu>
2018-11-30 09:35:23 +0100

    Improvements for CategoryProviderHook

--- src/hook/CategoryProviderHook.php
+++ src/hook/CategoryProviderHook.php
@@ -26,12 +26,12 @@ class CategoryProviderHook implements Hook
 
         if (!isset($data['categoryProviderName']) || empty($data['categoryProviderName'])) {
             $sth = $this->pdo->prepare(
-                "SELECT company_company_name FROM cp_categoryprovider WHERE id = :id"
+                "SELECT company_company_name, date_modified FROM cp_categoryprovider WHERE id = :id"
             );
             $sth->execute(['id' => $data['categoryProviderId']]);
-            $providerName = $sth->fetchColumn();
+            $provider = $sth->fetch(\PDO::FETCH_ASSOC);
 
-            if (empty($providerName)) {
+            if (empty($provider['company_company_name'])) {
                 $url = self::ENDPOINT . $data['categoryProviderId'];
                 $json = file_get_contents($url);
 
@@ -42,13 +42,13 @@ class CategoryProviderHook implements Hook
                 if (!empty($res)) {
                     $sth = $this->pdo->prepare(
                         "INSERT INTO cp_categoryprovider(
-                            id, category_provider_id, created, lastModified, category_id,
-                            category_created, category_lastModified, category_categoryType,
+                            id, category_provider_id, created, date_modified, category_id,
+                            category_created, category_lastmodified, category_category_type,
                             category_category_name, category_slug, category_icon,
                             category_sort_priority, category_description, company_id,
-                            company_created, company_lastModified, company_company_name,
+                            company_created, company_lastmodified, company_company_name,
                             company_external_id, company_company_source, company_slug,
-                            company_externalLogoUrl, company_imagePath, company_blacklisted,
+                            company_externallogourl, company_imagepath, company_blacklisted,
                             default_notice_period, active, sort_priority, disabled
                         ) VALUES (
                             :id, :category_provider_id, :created, :lastModified, :category_id,
@@ -95,7 +95,81 @@ class CategoryProviderHook implements Hook
                     $data['categoryProviderName'] = $res['company']['companyName'];
                 }
             } else {
-                $data['categoryProviderName'] = $providerName;
+                if ((strtotime($provider['date_modified']) + 3600) < time()) {
+                    $url = self::ENDPOINT . $data['categoryProviderId'];
+                    $json = file_get_contents($url);
+
+                    if (!empty($json)) {
+                        $res = json_decode($json, true);
+                    }
+
+                    if (!empty($res)) {
+                        $sth = $this->pdo->prepare(
+                            "UPDATE cp_categoryprovider SET 
+                                category_provider_id = :category_provider_id,
+                                created = :created,
+                                date_modified = :lastModified,
+                                category_id = :category_id, 
+                                category_created = :category_created, 
+                                category_lastmodified = :category_lastModified, 
+                                category_category_type = :category_categoryType, 
+                                category_category_name = :category_category_name, 
+                                category_slug = :category_slug, 
+                                category_icon = :category_icon, 
+                                category_sort_priority = :category_sort_priority, 
+                                category_description = :category_description, 
+                                company_id = :company_id, 
+                                company_created = :company_created, 
+                                company_lastmodified = :company_lastModified, 
+                                company_company_name = :company_company_name, 
+                                company_external_id = :company_external_id, 
+                                company_company_source = :company_company_source, 
+                                company_slug = :company_slug, 
+                                company_externallogourl = :company_externalLogoUrl, 
+                                company_imagepath = :company_imagePath, 
+                                company_blacklisted = :company_blacklisted, 
+                                default_notice_period = :default_notice_period, 
+                                active = :active, 
+                                sort_priority = :sort_priority, 
+                                disabled = :disabled 
+                            WHERE id = :id"
+                        );
+
+                        $sth->execute([
+                            'id' => $res['id'],
+                            'category_provider_id' => $res['id'],
+                            'created' => $res['created'],
+                            'lastModified' => date('Y-m-d H:i:s'),
+                            'category_id' => $res['category']['id'],
+                            'category_created' => $res['category']['created'],
+                            'category_lastModified' => $res['category']['lastModified'],
+                            'category_categoryType' => $res['category']['categoryType'],
+                            'category_category_name' => $res['category']['categoryName'],
+                            'category_slug' => $res['category']['slug'],
+                            'category_icon' => $res['category']['icon'],
+                            'category_sort_priority' => $res['category']['sortPriority'],
+                            'category_description' => $res['category']['description'],
+                            'company_id' => $res['company']['id'],
+                            'company_created' => $res['company']['created'],
+                            'company_lastModified' => $res['company']['lastModified'],
+                            'company_company_name' => $res['company']['companyName'],
+                            'company_external_id' => $res['company']['externalId'],
+                            'company_company_source' => $res['company']['companySource'],
+                            'company_slug' => $res['company']['slug'],
+                            'company_externalLogoUrl' => $res['company']['externalLogoUrl'],
+                            'company_imagePath' => $res['company']['imagePath'],
+                            'company_blacklisted' => $res['company']['blacklisted'],
+                            'default_notice_period' => $res['defaultNoticePeriod'],
+                            'active' => $res['active'],
+                            'sort_priority' => $res['sortPriority'],
+                            'disabled' => $res['disabled']
+                        ]);
+
+                        $provider['company_company_name'] = $res['company']['companyName'];
+                    }
+                }
+
+                $data['categoryProviderName'] = $provider['company_company_name'];
             }
         }
 

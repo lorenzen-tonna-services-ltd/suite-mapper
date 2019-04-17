ad00ff0f5a70956c49e1411d979274ccbcb487b0
j_lorenzen <jannik.lorenzen@modix.eu>
2019-04-17 19:58:19 +0100

    Add SSL-work-around and basic auth to http storage

--- src/storage/HttpStorage.php
+++ src/storage/HttpStorage.php
@@ -9,6 +9,16 @@ class HttpStorage implements Storage
     private $baseUrl;
 
     /**
+     * @var string
+     */
+    private $htUser;
+
+    /**
+     * @var string
+     */
+    private $htPassword;
+
+    /**
      * @param $baseUrl string Base URL
      */
     public function __construct($baseUrl)
@@ -27,6 +37,26 @@ class HttpStorage implements Storage
         throw new \Exception('HttpStorage does not support write actions.');
     }
 
+    public function getUser()
+    {
+        return $this->htUser;
+    }
+
+    public function setUser($user)
+    {
+        $this->htUser = $user;
+    }
+
+    public function getPassword()
+    {
+        return $this->htPassword;
+    }
+
+    public function setPassword($password)
+    {
+        $this->htPassword = $password;
+    }
+
     /**
      * @param string $file
      * @return string
@@ -36,6 +66,11 @@ class HttpStorage implements Storage
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $this->baseUrl . $file);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        if (!empty($this->getUser()) && !empty($this->getPassword())) {
+            curl_setopt($ch, CURLOPT_USERPWD, $this->htUser .':'. $this->htPassword);
+        }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         $json = trim(curl_exec($ch));
